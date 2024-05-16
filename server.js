6378086b0e0179d245280f3daa3d45c1b27ad298
Lorenzo Bologna <116873808+BolognaL@users.noreply.github.com>
2024-05-16 10:24:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,22 +6,21 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Lista dei giocatori attualmente connessi
 let players = [];
 let moves = {};
+let usernames = {};
 
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    // Aggiungi il nuovo giocatore alla lista dei giocatori
-    players.push(socket.id);
+    socket.on('startGame', (username) => {
+        players.push(socket.id);
+        usernames[socket.id] = username;
+        if (players.length === 2) {
+            io.emit('startGame', usernames);
+        }
+    });
 
-    // Se ci sono due giocatori connessi, avvia il gioco
-    if (players.length === 2) {
-        io.emit('startGame');
-    }
-
-    // Gestisci la scelta del giocatore e invia il risultato agli altri giocatori
     socket.on('play', (move) => {
         moves[socket.id] = move;
         if (Object.keys(moves).length === 2) {
@@ -31,26 +30,25 @@ io.on('connection', (socket) => {
             const result2 = getResult(player2Move, player1Move);
             io.to(players[0]).emit('result', result1);
             io.to(players[1]).emit('result', result2);
-            // Resetta le mosse per la prossima partita
             moves = {};
         }
     });
 
-    // Gestisci la disconnessione del giocatore
+    socket.on('disconnectGame', () => {
+        console.log('User disconnected from game');
+        players = players.filter(p => p !== socket.id);
+        if (players.length > 0) {
+            io.emit('playerDisconnected');
+        }
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
-        // Rimuovi il giocatore dalla lista dei giocatori
-        players = players.filter(p => p !== socket.id);
-        // Resetta le mosse se il giocatore che si è disconnesso aveva fatto una mossa
-        if (moves[socket.id]) {
-            delete moves[socket.id];
-        }
+        delete usernames[socket.id];
     });
 });
 
 function getResult(move1, move2) {
-    // Logic to determine the winner
-    // 0: Draw, 1: Player 1 wins, 2: Player 2 wins
     if (move1 === move2) return 0;
     if ((move1 === 0 && move2 === 2) ||
         (move1 === 1 && move2 === 0) ||
